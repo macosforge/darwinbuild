170481764cf944b5ed68adcb0f1b92784e2fb381
Kevin Van Vechten <kvv@apple.com>
2005-07-11 23:35:22 +0000

    - added common.mk to provide defaults for common variables
    git-svn-id: https://svn.macosforge.org/repository/darwinbuild/trunk@62 10a61168-4876-4dac-953b-31e694342555

---
+++ common.mk
@@ -0,0 +1,14 @@
+###
+### Common makefile variables potentially set by autoconf
+###
+PREFIX?=/usr/local
+DESTDIR?=
+
+###
+###
+BINDIR=$(DESTDIR)$(PREFIX)/bin
+DATDIR=$(DESTDIR)$(PREFIX)/share
+INSTALL=install
+INSTALL_EXE_FLAGS=-m 0755 -o root -g wheel
+INSTALL_DIR_FLAGS=$(INSTALL_EXE_FLAGS)
+INSTALL_DOC_FLAGS=-m 0644 -o root -g wheel
