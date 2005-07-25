bdce7032cca0d2d3ee719e4e1e11fba2f973137d
Kevin Van Vechten <kvv@apple.com>
2005-07-25 18:36:06 +0000

    - fixed warnings since DarwinPorts builds with -Wall (pguyot)
    git-svn-id: https://svn.macosforge.org/repository/darwinbuild/trunk@85 10a61168-4876-4dac-953b-31e694342555

--- darwintrace/darwintrace.c
+++ darwintrace/darwintrace.c
@@ -35,6 +35,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/param.h>
@@ -55,7 +56,7 @@ inline void __darwintrace_setup() {
 		__darwintrace_fd = open(path,
 		O_CREAT | O_WRONLY | O_APPEND,
 		0666);
-		fcntl(__darwintrace_fd, F_SETFD, 1); // close-on-exec
+		fcntl(__darwintrace_fd, F_SETFD, 1); /* close-on-exec */
 	  }
 	}
 #if DARWINTRACE_SHOW_PROCESS
@@ -69,12 +70,13 @@ inline void __darwintrace_setup() {
 #endif
 }
 
-// Log calls to open(2) into the file specified by DARWINTRACE_LOG.
-// Only logs if the DARWINTRACE_LOG environment variable is set.
-// Only logs files where the open succeeds.
-// Only logs files opened for read access, without the O_CREAT flag set.
-// The assumption is that any file that can be created isn't necessary
-// to build the project.
+/* Log calls to open(2) into the file specified by DARWINTRACE_LOG.
+   Only logs if the DARWINTRACE_LOG environment variable is set.
+   Only logs files where the open succeeds.
+   Only logs files opened for read access, without the O_CREAT flag set.
+   The assumption is that any file that can be created isn't necessary
+   to build the project.
+*/
 
 int open(const char* path, int flags, ...) {
 #define open(x,y,z) syscall(SYS_open, (x), (y), (z))
@@ -95,12 +97,12 @@ int open(const char* path, int flags, ...) {
 		    if(0 == fcntl(result, F_GETPATH, realpath)) {
 #if DARWINTRACE_SHOW_PROCESS
 		      size = snprintf(__darwintrace_buf, BUFFER_SIZE, "%s[%d]\topen\t%s\n", __darwintrace_progname, __darwintrace_pid, realpath );
-		      // printf("resolved %s to %s\n", path, realpath);
+		      /* printf("resolved %s to %s\n", path, realpath); */
 #else
 		      size = snprintf(__darwintrace_buf, BUFFER_SIZE, "open\t%s\n", realpath );
 #endif
 		    }
-		    // if we can't resolve it, ignore the volfs path
+		    /* if we can't resolve it, ignore the volfs path */
 		  } else {
 #if DARWINTRACE_SHOW_PROCESS
 		    size = snprintf(__darwintrace_buf, BUFFER_SIZE, "%s[%d]\topen\t%s\n", __darwintrace_progname, __darwintrace_pid, path );
@@ -118,7 +120,7 @@ int open(const char* path, int flags, ...) {
 int execve(const char* path, char* const argv[], char* const envp[]) {
 #define execve(x,y,z) syscall(SYS_execve, (x), (y), (z))
 	int result;
-
+	
 	__darwintrace_setup();
 	if (__darwintrace_fd >= 0) {
 	  struct stat sb;
@@ -128,31 +130,33 @@ int execve(const char* path, char* const argv[], char* const envp[]) {
 #else
 		int size = snprintf(__darwintrace_buf, BUFFER_SIZE, "execve\t%s\n", path );
 #endif
+		int fd;
+		
 		write(__darwintrace_fd, __darwintrace_buf, size);
 		fsync(__darwintrace_fd);
 		
-		int fd = open(path, O_RDONLY, 0);
+		fd = open(path, O_RDONLY, 0);
 		if (fd != -1) {
 			char buffer[MAXPATHLEN];
 			ssize_t bytes_read = read(fd, buffer, MAXPATHLEN);
 			if (buffer[0] == '#' && buffer[1] == '!') {
 				const char* interp = &buffer[2];
 				int i;
-				// skip past leading whitespace
+				/* skip past leading whitespace */
 				for (i = 2; i < (MAXPATHLEN-1); ++i) {
 					if (buffer[i] != ' ' && buffer[i] != '\t') {
 						interp = &buffer[i];
 						break;
 					}
 				}
-				// found interpreter (or ran out of data)
-				// skip until next whitespace, then terminate the string
+				/* found interpreter (or ran out of data)
+				 skip until next whitespace, then terminate the string */
 				for (; i < (MAXPATHLEN-1); ++i) {
 					if (buffer[i] == ' ' || buffer[i] == '\t' || buffer[i] == '\n') {
 						buffer[i] = 0;
 					}
 				}
-				// we have liftoff
+				/* we have liftoff */
 				if (interp) {
 #if DARWINTRACE_SHOW_PROCESS
 					const char* procname = strrchr(argv[0], '/') + 1;
