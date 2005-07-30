23d07d1006f6777a1996a10d37dcc1eefe1d6213
Kevin Van Vechten <kvv@apple.com>
2005-07-30 22:06:27 +0000

    - use build-wide setting if per-project setting not found
    git-svn-id: https://svn.macosforge.org/repository/darwinbuild/trunk@136 10a61168-4876-4dac-953b-31e694342555

--- darwinxref/plugins/binary_sites.tcl
+++ darwinxref/plugins/binary_sites.tcl
@@ -9,7 +9,12 @@ proc usage {} {
 proc run {args} {
 	set project [lindex $args 0]
 	set build [DBGetCurrentBuild]
-	foreach site [DBCopyPropArray $build $project binary_sites] {
+	set sites [DBCopyPropArray $build $project binary_sites]
+	if {$project != "" && [llength $sites] == 0} {
+		# use build-wide settings if project settings not found
+		set sites [DBCopyPropArray $build "" binary_sites]
+	}
+	foreach site $sites {
 		puts "$site"
 	}
 }
