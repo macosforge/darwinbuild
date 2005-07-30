f0619e46f1c2359f4e6a8d7cde4741ad86723a31
Shantonu Sen <ssen@apple.com>
2005-07-30 06:24:27 +0000

    add plist_sites plugin, used to populate initial database
    git-svn-id: https://svn.macosforge.org/repository/darwinbuild/trunk@124 10a61168-4876-4dac-953b-31e694342555

---
+++ darwinxref/plugins/plist_sites.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2005 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_BSD_LICENSE_HEADER_START@
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer. 
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution. 
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * @APPLE_BSD_LICENSE_HEADER_END@
+ */
+
+#include "DBPlugin.h"
+
+static int run(CFArrayRef argv) {
+	CFIndex i, count = CFArrayGetCount(argv);
+	CFStringRef build = DBGetCurrentBuild();
+
+	if (count != 0)  return -1;
+
+	CFArrayRef plist_sites = DBCopyPropArray(build, NULL, CFSTR("plist_sites"));
+	count = plist_sites ? CFArrayGetCount(plist_sites) : 0;
+	for (i = 0; i < count; ++i) {
+		cfprintf(stdout, "%@\n", CFArrayGetValueAtIndex(plist_sites, i));
+	}
+	return 0;
+}
+
+static CFStringRef usage() {
+	return CFRetain(CFSTR(""));
+}
+
+int initialize(int version) {
+	//if ( version < kDBPluginCurrentVersion ) return -1;
+	
+	DBPluginSetType(kDBPluginPropertyType);
+	DBPluginSetName(CFSTR("plist_sites"));
+	DBPluginSetRunFunc(&run);
+	DBPluginSetUsageFunc(&usage);
+	DBPluginSetDataType(CFArrayGetTypeID());
+	return 0;
+}
