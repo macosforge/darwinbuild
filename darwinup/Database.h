bf9525a25f23566e9bb51db5488aa3da742f747d
wsiegrist@apple.com <wsiegrist@apple.com@localhost>
2010-01-29 21:53:20 +0000

    Initial work on new database abstraction API
    git-svn-id: https://svn.macosforge.org/repository/darwinbuild/branches/PR-7489777@677 10a61168-4876-4dac-953b-31e694342555

---
+++ darwinup/Database.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2010 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_BSD_LICENSE_HEADER_START@
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @APPLE_BSD_LICENSE_HEADER_END@
+ */
+
+#include <sqlite3.h>
+
+/**
+ * 
+ * Generic sqlite abstraction
+ *
+ */
+struct Database {
+	Database();
+	Database(const char* path);
+	virtual ~Database();
+
+	const char* path();
+	bool connect(const char* path);
+	
+	const char* get_value(const char* table, const char* column, const char* where);
+	const char* get_row(const char* table, const char* where);
+	const char* get_column(const char* table, const char* column, const char* where);
+	const char* get_all(const char* table, const char* where);
+	
+	int count(const char* table, const char* where);
+	
+	bool update(const char* table, const char* set, const char* where, int &count);
+	bool del(const char* table, const char* where, int &count);
+	bool insert(const char* table, const char* columns, const char* values);
+	
+	bool begin_transaction();
+	bool rollback_transaction();
+	bool commit_transaction();
+	
+protected:
+	
+	boot sql(const char* query, ...);
+	
+	char*         m_path;
+	sqlite3*      m_db;
+	Table**       m_tables;
+
+};
+
+/**
+ *
+ * Darwinup database abstraction. This class is responsible
+ *  for generating the Table and Column objects that make
+ *  up the darwinup database schema. 
+ *
+ */
+struct DarwinupDatabase : Database {
+	DarwinupDatabase();
+	DarwinupDatabase(const char* path);
+	virtual ~DarwinupDatabase();
+	
+};
+
