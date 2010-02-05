d56cf71cda1e7fb1f1f3a21c08cb269c2afee5fd
wsiegrist@apple.com <wsiegrist@apple.com@localhost>
2010-02-05 20:45:26 +0000

    Add test case for broken symlinks in destination needing backup. Add check that /d/file still exists after full uninstall due to user modifications.
    git-svn-id: https://svn.macosforge.org/repository/darwinbuild/branches/PR-7529688@692 10a61168-4876-4dac-953b-31e694342555

--- testing/darwinup/run-tests.sh
+++ testing/darwinup/run-tests.sh
@@ -11,7 +11,7 @@ ORIG=$PREFIX/orig
 DEST=$PREFIX/dest
 DESTTAR=dest.tar.gz
 
-DIFF="diff -x .DarwinDepot -qru"
+DIFF="diff -x .DarwinDepot -x broken -qru"
 
 ROOTS="root root2 root3"
 
@@ -27,7 +27,7 @@ do
 	tar zxvf $R.tar.gz -C $PREFIX
 done;
 
-for R in root4 root5 root6 root7;
+for R in root4 root5 root6 root7 symlinks;
 do
 	tar zxvf $R.tar.gz -C $PREFIX
 done;
@@ -35,7 +35,7 @@ done;
 mkdir -p $ORIG
 cp -R $DEST/* $ORIG/
 
-echo "TEST: Trying roots one at a time ..."
+echo "========== TEST: Trying roots one at a time =========="
 for R in $ROOTS;
 do
 	echo "INFO: Installing $R ...";
@@ -47,7 +47,7 @@ do
 	$DIFF $ORIG $DEST 2>&1
 done
 
-echo "TEST: Trying all roots at once, uninstall in reverse ...";
+echo "========== TEST: Trying all roots at once, uninstall in reverse ==========";
 for R in $ROOTS;
 do
 	echo "INFO: Installing $R ...";
@@ -62,7 +62,7 @@ done
 echo "DIFF: diffing original test files to dest (should be no diffs) ..."
 $DIFF $ORIG $DEST 2>&1
 
-echo "TEST: Trying all roots at once, uninstall in install order ..."
+echo "========== TEST: Trying all roots at once, uninstall in install order =========="
 for R in $ROOTS;
 do
         echo "INFO: Installing $R ...";
@@ -77,7 +77,7 @@ done
 echo "DIFF: diffing original test files to dest (should be no diffs) ..."
 $DIFF $ORIG $DEST 2>&1
 
-echo "TEST: Trying all roots at once, uninstall root2, root3, root ..."
+echo "========== TEST: Trying all roots at once, uninstall root2, root3, root =========="
 for R in $ROOTS;
 do
         echo "INFO: Installing $R ...";
@@ -92,7 +92,7 @@ done
 echo "DIFF: diffing original test files to dest (should be no diffs) ..."
 $DIFF $ORIG $DEST 2>&1
 
-echo "TEST: Trying roots in reverse, uninstall in install order ..."
+echo "========== TEST: Trying roots in reverse, uninstall in install order =========="
 for R in root3 root2 root;
 do
         echo "INFO: Installing $R ...";
@@ -107,7 +107,7 @@ done
 echo "DIFF: diffing original test files to dest (should be no diffs) ..."
 $DIFF $ORIG $DEST 2>&1
 
-echo "TEST: Try uninstalling with user data in rollback"
+echo "========== TEST: Try uninstalling with user data in rollback =========="
 echo "INFO: Installing root5 ...";
 darwinup -vv -p $DEST install $PREFIX/root5
 darwinup -vv -p $DEST install $PREFIX/root6
@@ -116,11 +116,19 @@ darwinup -vv -p $DEST install $PREFIX/root7
 darwinup -vv -p $DEST uninstall root6
 darwinup -vv -p $DEST uninstall root5
 darwinup -vv -p $DEST uninstall root7
+stat $DEST/d/file
+rm $DEST/d/file
+rmdir $DEST/d
 
-
+echo "========== TEST: Testing broken symlink handling =========="
+darwinup -vv -p $DEST install $PREFIX/symlinks
+darwinup -vv -p $DEST uninstall symlinks
+echo "DIFF: diffing original test files to dest (should be no diffs) ..."
+$DIFF $ORIG $DEST 2>&1
+ 
+# expected failures
 set +e
-
-echo "TEST: Trying a root that will fail due to object change ..."
+echo "========== TEST: Trying a root that will fail due to object change =========="
 darwinup -vv -p $DEST install $PREFIX/root4
 if [ $? -ne 1 ]; then exit 1; fi
 echo "DIFF: diffing original test files to dest (should be no diffs) ..."
