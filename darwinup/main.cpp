6a060f9fd97eee84a4ba10e6256cecce655fcf0d
wsiegrist@apple.com <wsiegrist@apple.com@localhost>
2010-05-05 03:19:46 +0000

    Changes and version bump for 18.1
    git-svn-id: https://svn.macosforge.org/repository/darwinbuild/trunk@826 10a61168-4876-4dac-953b-31e694342555

--- darwinup/main.cpp
+++ darwinup/main.cpp
@@ -46,7 +46,7 @@
 
 void usage(char* progname) {
 	fprintf(stderr, "usage:    %s [-v] [-p DIR] [command] [args]          \n", progname);
-	fprintf(stderr, "version: 18                                                    \n");
+	fprintf(stderr, "version: 18.1                                                  \n");
 	fprintf(stderr, "                                                               \n");
 	fprintf(stderr, "options:                                                       \n");
 #if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1060
