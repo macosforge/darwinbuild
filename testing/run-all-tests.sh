71318e29c97ed20a9027298a5121640d54212de3
wsiegrist@apple.com <wsiegrist@apple.com@localhost>
2011-02-08 01:44:31 +0000

    Only run tests in directories
    git-svn-id: https://svn.macosforge.org/repository/darwinbuild/branches/PR-8817822@921 10a61168-4876-4dac-953b-31e694342555

--- testing/run-all-tests.sh
+++ testing/run-all-tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
-set -x
 pushd $(dirname $0) >> /dev/null
 
 for X in *;
 do
-	$X/run-tests.sh
+	if [ -d $X ]; then
+		$X/run-tests.sh
+	fi
 done
+
+echo "INFO: All testing completed!"
