6db02c7b6a34a80f062730061507e89f2af1d0dd
William Kent <wjk@users.noreply.github.com>
2017-11-14 14:53:04 -0500

    Modernize darwintrace redirection exemption path list

--- darwintrace/darwintrace.c
+++ darwintrace/darwintrace.c
@@ -79,14 +79,7 @@ static pid_t darwintrace_pid = -1;
 static char *darwintrace_redirect = NULL; 
 static char *darwintrace_buildroot = NULL;
 static const char *darwintrace_exceptions[] = {
-	"/Developer/Library/Private",
-	"/Developer/Library/Frameworks",
-	"/Developer/usr/bin/../../Library/Private",
-	"/Developer/usr/bin/../../Library/Frameworks",
-	"/Developer/Library/Xcode",
-	"/Developer/Platforms/",
-	"/Developer/usr/bin/xcode",
-	"/System/Library/Frameworks/Carbon",
+	"/Applications/Xcode.app/Contents/Developer",
 	"/Volumes/BuildRoot_",
 	"/usr/bin/xcrun",
 	"/usr/bin/xcode",
