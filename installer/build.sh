433b6b6eb2af3ea6f7d7b96601fb27c8f0891ec1
William Kent <wjk@users.noreply.github.com>
2018-12-06 19:12:39 -0500

    Add files to create installer

---
+++ installer/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+MY_DIR=$(cd `dirname $0` && pwd)
+cd $MY_DIR
+
+xcodebuild install \
+	-project $MY_DIR/../darwinbuild.xcodeproj \
+	-target world \
+	DSTROOT=$MY_DIR/payload
+
+pkgbuild \
+	--ownership recommended \
+	--identifier org.puredarwin.darwinbuild.component \
+	--version 1.0 \
+	--root $MY_DIR/payload \
+	--install-location / \
+	$MY_DIR/darwinbuild-component.pkg
+
+productbuild \
+	--distribution $MY_DIR/distribution.xml \
+	--identifier org.puredarwin.darwinbuild.release \
+	--version 1.0 \
+	--sign 'Developer ID Installer' --timestamp \
+	--package-path $MY_DIR \
+	--resources $MY_DIR \
+	$MY_DIR/darwinbuild-installer.pkg
+
+echo "Complete! Your installer is located at: $MY_DIR/build/darwinbuild-installer.pkg"
