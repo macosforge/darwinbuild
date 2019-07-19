d9986538c950704a0606179e721d20a6aba0dc0a
William Kent <wjk011@gmail.com>
2019-07-18 20:21:14 -0400

    Rewrite instller/notarize.sh
    The script now polls for the notarization status, instead
    of requiring the user to do that manually.

--- installer/notarize.sh
+++ installer/notarize.sh
@@ -5,26 +5,64 @@ set -e
 MY_DIR=$(cd `dirname $0` && pwd)
 cd $MY_DIR
 
-if [ "$1" == "-upload" ]; then
-	if [ ! -f $MY_DIR/darwinbuild-installer.pkg ]; then
-		echo "darwinbuild-installer.pkg not present, please run build.sh to create it" 1>&2
+if [ ! -f $MY_DIR/darwinbuild-installer.pkg ]; then
+	echo "darwinbuild-installer.pkg not present, please run build.sh to create it" 1>&2
+	exit 1
+fi
+
+ALTOOL_LOG=$(mktemp -q -t altool.log)
+if [ $? -ne 0 ]; then
+	echo "Could not create temporary file for altool log" 1>&2
+	exit 1
+fi
+
+xcrun altool --notarize-app --primary-bundle-id org.puredarwin.darwinbuild.release \
+	--username "wjk011@gmail.com" --password "@keychain:ADC Notarization" \
+	--file $MY_DIR/darwinbuild-installer.pkg \
+	--output-format xml > $ALTOOL_LOG
+if [ $? -ne 0 ]; then
+	echo "altool failed to upload, cannot continue" 1>&2
+	echo "altool log file is located at: $ALTOOL_LOG" 1>&2
+	exit 1
+fi
+
+request_id=$(/usr/libexec/PlistBuddy -c "print :notarization-upload:RequestUUID" $ALTOOL_LOG)
+if [[ $request_id =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
+	attempts=5
+
+	while :; do
+		echo "Sleeping 20 seconds before checking notarization status..."
+		sleep 20
+
+		xcrun altool --notarization-info $request_id \
+			--username "wjk011@gmail.com" --password "@keychain:ADC Notarization" \
+			--output-format xml > $ALTOOL_LOG
+
+		notarization_status=$(/usr/libexec/PlistBuddy -c "print :notarization-info:Status" $ALTOOL_LOG)
+
+		if [ ! -z "$notarization_status" ]; then
+			[ "$notarization_status" != "in progress" ] && break
+		else
+			(($attempts <= 0)) && break
+			((attempts--))
+		fi
+	done
+
+	if [ "$status" != "success" ]; then
+		echo "altool reported notarization error" 1>&2
 		exit 1
 	fi
 
-	exec xcrun altool --notarize-app --primary-bundle-id org.puredarwin.darwinbuild.release \
-		--username "wjk011@gmail.com" --password "@keychain:ADC Notarization" \
-		--file $MY_DIR/darwinbuild-installer.pkg
-elif [ "$1" == "-staple" ]; then
-	if [ ! -f $MY_DIR/darwinbuild-installer.pkg ]; then
-		echo "darwinbuild-installer.pkg not present, please run build.sh to create it" 1>&2
+	log_url=$(/usr/libexec/PlistBuddy -c "print :notarization-info:LogFileURL" $ALTOOL_LOG)
+	echo "Log file can be downloaded from: $log_url"
+
+	xcrun stapler staple $MY_DIR/darwinbuild-installer.pkg
+	xcrun stapler validate -v $MY_DIR/darwinbuild-installer.pkg
+	if [ $@ -ne 0 ]; then
+		echo "validation of installer notarization failed" 1>&2
 		exit 1
 	fi
-
-	exec xcrun stapler staple $MY_DIR/darwinbuild-installer.pkg
-elif [ "$1" == "-status" ]; then
-	exec xcrun altool --notarization-history 0 -u "wjk011@gmail.com" -p "@keychain:ADC Notarization"
 else
-	echo "usage: $0 { -upload | -staple | -status }" 1>&2
-	echo "Wraps altool to ease notarization of the darwinbuild installer package" 1>&2
+	echo "Invalid request ID found in altool output, cannot continue." 1>&2
 	exit 1
 fi
