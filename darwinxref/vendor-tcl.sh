51ca0c250c998d483891ed36b032ae4972237233
William Kent <wjk011@gmail.com>
2020-08-03 22:19:14 -0400

    Install vendored copy of libtcl to share/darwinxref

--- darwinxref/vendor-tcl.sh
+++ darwinxref/vendor-tcl.sh
@@ -3,12 +3,12 @@
 if [ ! -f $BUILT_PRODUCTS_DIR/libtcl8.6.dylib ]; then
 	cp /usr/local/opt/tcl-tk/lib/libtcl8.6.dylib $BUILT_PRODUCTS_DIR
 	chmod u+w $BUILT_PRODUCTS_DIR/libtcl8.6.dylib
-	install_name_tool -id '@loader_path/../share/darwinbuild/libtcl8.6.dylib' $BUILT_PRODUCTS_DIR/libtcl8.6.dylib
+	install_name_tool -id '@loader_path/../share/darwinxref/libtcl8.6.dylib' $BUILT_PRODUCTS_DIR/libtcl8.6.dylib
 fi
 
 if [ "$ACTION" = "install" ]; then
-	mkdir -p $DSTROOT/usr/local/share/darwinbuild
+	mkdir -p $DSTROOT/usr/local/share/darwinxref
 	install -o $INSTALL_OWNER -g $INSTALL_GROUP -m $INSTALL_MODE_FLAG \
 		$BUILT_PRODUCTS_DIR/libtcl8.6.dylib \
-		$DSTROOT/usr/local/share/darwinbuild
+		$DSTROOT/usr/local/share/darwinxref
 fi
